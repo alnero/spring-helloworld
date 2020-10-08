8642a821611770bd6e715b0cf786ef7e6c8d230c
alnero <darestream@gmail.com>
2020-10-08 12:13:26 +0300

    Add cat bean, uncomment test

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,11 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld helloWorldBean1 = applicationContext.getBean("helloworld", HelloWorld.class);
+        HelloWorld helloWorldBean2 = applicationContext.getBean("helloworld", HelloWorld.class);
+        Cat catBean1 = applicationContext.getBean("cat", Cat.class);
+        Cat catBean2 = applicationContext.getBean("cat", Cat.class);
+        System.out.println("Compare helloworld beans: " + (helloWorldBean1 == helloWorldBean2));
+        System.out.println("Compare cat beans: " + (catBean1 == catBean2));
     }
 }
